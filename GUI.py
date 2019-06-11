cdb7e975519f6c01215949d66bbe721fbab41e21
Xiang <1073739700@qq.com>
2019-06-12 01:38:57 +0800

    GUI & backEnd - cd_in & start program

--- GUI.py
+++ GUI.py
@@ -1,5 +1,4 @@
 # -*- coding: UTF-8 -*-
- 
 from tkinter import ttk
 from tkinter import *
 from backEnd import *
@@ -10,41 +9,41 @@ class MainWindow():
         self.disk = OSManager(user)
         # 生成窗体
         self.win = Tk()
-        self.win.title('File Management System')
+        # self.win.title('Simulated File Management System')
+        self.win.title('模拟文件系统')
         self.win.geometry('800x350')
         self.win.resizable(width=False, height=False)
         ft = ('Microsoft YaHei', 10)  # 全局字体
         # 顶部label
-        self.l_author = Label(self.win, text = self.disk.here.author, 
+        self.l_user = Label(self.win, text = self.disk.here.user.name, 
                             bg = 'Plum', font = ft)
         self.l_now = Label(self.win, text = '当前目录：', 
                             bg = 'Lavender', font = ft)
-        self.l_dir_text = StringVar()
+        self.l_dir_text = StringVar()  # 动态更新当前路径
         self.l_dir_text.set(self.disk.here.path + self.disk.here.name)
         self.l_dir = Label(self.win, textvariable = self.l_dir_text, 
                             bg = 'LightCyan', font = ft, width = 90, anchor = 'w')
-
         # 操作按钮
-        self.ctrl = Frame(self.win)
-        self.back = Button(self.ctrl, text = '<--', font = ft, command = self.cd_back)                
+        self.ctrl = Frame(self.win)  # 控制面板，用于布局
+        self.back = Button(self.ctrl, text = '<--', font = ft, command = self.cd_back)
         self.add_folder = Button(self.ctrl, text = '新建文件夹', font = ft)
         self.add_file = Button(self.ctrl, text = '新建文件', font = ft)
         self.delete = Button(self.ctrl, text = '删除', font = ft)
-        # 文件信息表格初始化
+        # 文件信息表初始化
         self.ftree = ttk.Treeview(self.win, show = 'headings')
         self.ftree['columns'] = ('fname', 'fdate', 'ftype', 'fsize', 'fauthor')
-        self.ftree.column('fname', width = 270)
+        self.ftree.column('fname', width = 268)
         self.ftree.column('fdate', width = 150)
         self.ftree.column('ftype', width = 80)
         self.ftree.column('fsize', width = 80)
         self.ftree.column('fauthor', width=80)
-        self.ftree.heading('fname', text = '文件名')
-        self.ftree.heading('fdate', text = '修改日期')
-        self.ftree.heading('ftype', text = '类型')
-        self.ftree.heading('fsize', text = '大小')
-        self.ftree.heading('fauthor', text = '拥有者')
+        self.ftree.heading('fname', text = '文件名', anchor = 'w')
+        self.ftree.heading('fdate', text = '修改日期', anchor = 'w')
+        self.ftree.heading('ftype', text = '类型', anchor = 'w')
+        self.ftree.heading('fsize', text = '大小', anchor = 'w')
+        self.ftree.heading('fauthor', text = '拥有者', anchor = 'w')
         # 设置布局
-        self.l_author.grid(row = 0, column = 0, sticky = W)
+        self.l_user.grid(row = 0, column = 0, sticky = W)
         self.l_now.grid(row = 1, column = 0, sticky = W)
         self.l_dir.grid(row = 1, column = 1, sticky = W)
         self.ctrl.grid(row = 2, column = 1, sticky = W)
@@ -53,36 +52,51 @@ class MainWindow():
         self.add_file.grid(row = 0, column = 2, sticky = W)
         self.delete.grid(row = 0, column = 3, sticky = W)
         self.ftree.grid(row = 3, column = 1, columnspan = 5)
+        # 展示文件信息
         self.ls()
 
     # 展示当前目录下属于当前用户的所有文件
     def ls(self):
+        print('>> ls')
         fname, fdate, ftype, fsize, fauthor = self.disk.ls()
-        print(fname)
         for i in range(len(fname)):
             self.ftree.insert('', i, text='', 
                 values=(fname[i], fdate[i], ftype[i], fsize[i], fauthor[i]))
 
-    # 进入下一级
-    def cd_in(self):
-        pass
+    # 双击鼠标左键 - 进入下一级 或 运行目标程序
+    def cd_in(self, event):
+        for item in self.ftree.selection():
+            item_text = self.ftree.item(item,"values")
+            if self.disk.cd_in(item_text[0]):
+                print('>> cd ' + item_text[0])
+                self.refesh()
+            else:
+                print('>> start ', item_text[0])
 
-    # 返回上一级
+    # 点击返回按钮 - 返回上一级
     def cd_back(self):
-        self.disk.cd_back()
+        if self.disk.here._type == 'root':
+            print('>> 已到根目录')
+        else:
+            print('>> cd ..')
+            self.disk.cd_back()
+            self.refesh()
+    
+    # 刷新
+    def refesh(self):
         x = self.ftree.get_children()
         for item in x:
             self.ftree.delete(item)
         self.l_dir_text.set(self.disk.here.path + self.disk.here.name)
         self.ls()
-        self.win.update()
 
-    # 运行窗口
+    # 绑定事件，运行窗口
     def show(self):
+        self.ftree.bind('<Double-1>', self.cd_in)
         self.win.mainloop()
 
 
 if __name__ == "__main__":
-    user = User('Tony', 'rw')
-    mw = MainWindow(user)
-    mw.show()
+    user = User('root', 'rw')
+    main_window = MainWindow(user)
+    main_window.show()
--- backEnd.py
+++ backEnd.py
@@ -2,12 +2,11 @@
 import os
 import datetime
 import time
-import tkinter
-
 
 # 文件控制块
 class FCB():
-    def __init__(self, parent, name, author, date):
+    def __init__(self, user, parent, name, author, date):
+        self.user = user
         self.parent = parent  # parent必须是一个文件夹
         self.name = name
         self.author = author
@@ -16,7 +15,10 @@ class FCB():
 
     # 重命名
     def rename(self, name):
-        self.name = name
+        src_path = self.path + self.name
+        dst_name = self.path + name
+        with os.rename(src_path, dst_name):
+            self.name = name
     
     # 返回上一级
     def back(self):
@@ -31,18 +33,18 @@ class FCB():
                 line = f.readline()
         for root, dirs, files in os.walk(parent_path):  
             for i in dirs:
-                if self.author == f_author[i] or self.author == 'root':
+                if self.user.name == f_author[i] or self.user.name == 'root':
                     path = self.parent.path  # 获取最后修改时间
                     time_stamp = os.path.getmtime(path)
                     time_array = time.localtime(time_stamp)
-                    a = Folder(self.parent, i, f_author[i], time.strftime('%Y-%m-%d %H:%M:%S', time_array))
+                    a = Folder(self.user, self.parent, i, f_author[i], time.strftime('%Y-%m-%d %H:%M:%S', time_array))
                     parent_list.append(a)  # 将文件夹对象加入子节点
             for i in files:
-                if self.author == f_author[i] or self.author == 'root':
+                if self.user.name == f_author[i] or self.user.name == 'root':
                     path = self.parent.path
                     time_stamp = os.path.getmtime(path)
                     time_array = time.localtime(time_stamp)
-                    a = File(self.parent, i, f_author[i], time.strftime('%Y-%m-%d %H:%M:%S', time_array))
+                    a = File(self.user, self.parent, i, f_author[i], time.strftime('%Y-%m-%d %H:%M:%S', time_array))
                     parent_list.append(a)  # 将文件对象加入子节点
             break
         return parent_list
@@ -55,35 +57,46 @@ class Folder(FCB):
 
     # 获取当前文件夹下的所有子文件
     def get_children(self):
-        for root, dirs, files in os.walk(self.path + self.name):  
+        f_author = {}
+        children_path = self.path + self.name
+        with open(children_path + '/config.txt', 'r') as f:
+            line = f.readline()
+            while line:
+                line_tmp = line.strip('\n').split(' ')
+                f_author[line_tmp[0]] = line_tmp[1]
+                line = f.readline()
+        for root, dirs, files in os.walk(children_path):  
             # 若用户能看到此文件夹，则必为该文件夹的拥有者，因此无需做用户检查
             for i in dirs:
-                path = self.path + self.name + '/' + i  # 获取最后修改时间
-                time_stamp = os.path.getmtime(path)
-                time_array = time.localtime(time_stamp)
-                a = Folder(self, i, self.author, time.strftime('%Y-%m-%d %H:%M:%S', time_array))
-                self.children.append(a)  # 将文件夹对象加入子节点
+                if self.user.name == f_author[i] or self.user.name == 'root':
+                    path = self.path + self.name + '/' + i  # 获取最后修改时间
+                    time_stamp = os.path.getmtime(path)
+                    time_array = time.localtime(time_stamp)
+                    a = Folder(self.user, self, i, self.author, time.strftime('%Y-%m-%d %H:%M:%S', time_array))
+                    self.children.append(a)  # 将文件夹对象加入子节点
             for i in files:
-                path = self.path + self.name + '/' + i
-                time_stamp = os.path.getmtime(path)
-                time_array = time.localtime(time_stamp)
-                a = File(self, i, self.author, time.strftime('%Y-%m-%d %H:%M:%S', time_array))
-                self.children.append(a)  # 将文件对象加入子节点
+                if self.user.name == f_author[i] or self.user.name == 'root':
+                    path = self.path + self.name + '/' + i
+                    time_stamp = os.path.getmtime(path)
+                    time_array = time.localtime(time_stamp)
+                    a = File(self.user, self, i, self.author, time.strftime('%Y-%m-%d %H:%M:%S', time_array))
+                    self.children.append(a)  # 将文件对象加入子节点
             break
         return self.children
 
 
 # 文件
 class File(FCB):
-    def __init__(self, parent, name, author, date):
-            self.parent = parent  # parent必须是一个文件夹
-            self.name = name  # 文件名
-            self.author = author  # 文件所有者
-            self.date = date  # 文件最后修改时间
-            self.path = parent.path + parent.name + '/'  # 文件路径
-            t = self.name.split('.')
-            self._type = t[len(t)-1]  # 文件类型
-            self.size = os.path.getsize(self.path + self.name)  # 文件大小
+    def __init__(self, user, parent, name, author, date):
+        self.user = user  # 当前用户
+        self.parent = parent  # parent必须是一个文件夹
+        self.name = name  # 文件名
+        self.author = author  # 文件所有者
+        self.date = date  # 文件最后修改时间
+        self.path = parent.path + parent.name + '/'  # 文件路径
+        t = self.name.split('.')
+        self._type = t[len(t)-1]  # 文件类型
+        self.size = os.path.getsize(self.path + self.name)  # 文件大小
 
     # def read(self):
     #     with open(self.path, 'r') as f:
@@ -92,12 +105,12 @@ class File(FCB):
 
 # 根节点
 class Root():
-    def __init__(self, author):
+    def __init__(self, user):
         self.name = ''
-        self.type = 'root'
+        self._type = 'root'
         self.path = 'root'
         self.children = []
-        self.author = author
+        self.user = user
     
     # 系统初始化装载
     def load(self):
@@ -110,18 +123,18 @@ class Root():
                 line = f.readline()
         for root, dirs, files in os.walk('root'):  
             for i in dirs:
-                if self.author == f_author[i] or self.author == 'root':
+                if self.user.name == f_author[i] or self.user.name == 'root':
                     path = self.path + '/' + i  # 获取最后修改时间
                     time_stamp = os.path.getmtime(path)
                     time_array = time.localtime(time_stamp)
-                    a = Folder(self, i, f_author[i], time.strftime('%Y-%m-%d %H:%M:%S', time_array))
+                    a = Folder(self.user, self, i, f_author[i], time.strftime('%Y-%m-%d %H:%M:%S', time_array))
                     self.children.append(a)  # 将文件夹对象加入子节点
             for i in files:
-                if self.author == f_author[i] or self.author == 'root':
+                if self.user.name == f_author[i] or self.user.name == 'root':
                     path = self.path + '/' + i
                     time_stamp = os.path.getmtime(path)
                     time_array = time.localtime(time_stamp)
-                    a = File(self, i, f_author[i], time.strftime('%Y-%m-%d %H:%M:%S', time_array))
+                    a = File(self.user, self, i, f_author[i], time.strftime('%Y-%m-%d %H:%M:%S', time_array))
                     self.children.append(a)  # 将文件对象加入子节点
             break
         return self.children
@@ -130,16 +143,13 @@ class Root():
 # 控制程序
 class OSManager():
     def __init__(self, user):
-        root = Root(user.name)
+        root = Root(user)
         self.user = user
         self.main_board = root.load()
         self.here = root
-        self.cd_in('测试文件夹1')
 
     # 展示当前目录下属于当前用户的所有文件
     def ls(self):
-        print('>>> ls')
-        print('当前目录：', self.here.name)
         name = []
         date = []
         _type = []
@@ -159,19 +169,22 @@ class OSManager():
         return name, date, _type, size, author
                 
             
-    # 进入下一级
+    # 进入下一级 或 运行目标程序
     def cd_in(self, target_name):
-        print('>>> cd ' + target_name)
         for item in self.main_board:
             if item.name == target_name:
-                self.here = item
-                self.main_board.clear()
-                self.main_board = item.get_children()
-                break
+                if item._type == 'folder':  # 若为目录，则进入下一级
+                    self.here = item
+                    self.main_board.clear()
+                    self.main_board = item.get_children()
+                    return True
+                else:  # 若为可执行程序，则运行程序
+                    target_path = item.path + target_name
+                    os.system('start ' + target_path)
+                    return False
     
     # 返回上一级
     def cd_back(self):
-        print('>>> cd ..')
         self.main_board.clear()
         self.main_board = self.here.back()
         self.here = self.here.parent
